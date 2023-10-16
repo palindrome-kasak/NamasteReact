95a3e5efc8adb803fcba53f9165f0fef02bd9a8f
Kasak Gupta <64642473+palindrome-kasak@users.noreply.github.com>
2023-10-17 00:47:01 +0530

    cc

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
+// React element==> object
 
-const heading = React.createElement("h1",{id:"heading"},"Hello worlld from React");
+const jsxHeading = <h1 id="heading"> namste using jsx</h1>
+
+// React functional compenent
+const Title =()=>{
+    <h1 className="head">i am girl</h1>
+};
+const HeadingComp = ()=>(
+    <div id="jd"> 
+    <Title/>
+     <h1 className="hjgd">namste funcyional component</h1>;
+     </div>
+);
+
+const heading = React.createElement("h1", { id: "heading" }, "Namsate React tom");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComp/>);
+
