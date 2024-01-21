2ccc64b42ae7d74d9c63c64c6a92fa84c4a64252
Kasak Gupta <64642473+palindrome-kasak@users.noreply.github.com>
2024-01-22 00:30:37 +0530

    cc , React use State hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,32 @@
-import ResturantCard from './ResturantCard';
+import ResturantCard from "./ResturantCard";
 import resObj from '../utils/mockData'
+import { useState } from "react";
 
-
+// super powerfull variable , react hook use State => it is a normal javascipt fucntion which is given to us by React => local state variable
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">search</div>
-        <div className="res-container">
-          {/* resturant card */}
-          {resObj.map((restur) => (
-            <ResturantCard key={restur.info.id} resData={restur} />
-          ))}
-        </div>
+  const [listOfRes, setlistOfRes] = useState(resObj);
+  return (
+    <div className="body">
+      {/* <div className="search">search</div> */}
+      <div className="btn-div">
+        <button className="filter-bt" onClick={ ()=>{
+          const FilteredList = listOfRes.filter((res)=>res.info.avgRating>4);
+          setlistOfRes(FilteredList);
+        }
+        }>
+          top Rated res
+        </button>
       </div>
-    );
-  };
+
+      <div className="res-container">
+        {/* resturant card */}
+        {listOfRes.map((restur) => (
+          <ResturantCard key={restur.info.id} resData={restur} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
