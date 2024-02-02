b1c2efb6bce85416e57015a02c003287c771bb7a
Kasak Gupta <64642473+palindrome-kasak@users.noreply.github.com>
2024-02-03 01:05:47 +0530

    seacrh functinality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,41 +1,76 @@
 import ResturantCard from "./ResturantCard";
-import ShimmerUI from './ShimmerUI'
+import ShimmerUI from "./ShimmerUI";
 import { useState, useEffect } from "react";
-
 
 // super powerfull variable , react hook use State => it is a normal javascipt fucntion which is given to us by React => local state variable
 
 const Body = () => {
-  
   const [listOfRes, setlistOfRes] = useState([]);
+  const [FileterlistOfRes, setFileterlistOfRes] = useState([]);
+  const [searchText, setsearchText] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[]);
-  const fetchData =async()=>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const jsonData = await data.json();
     console.log(jsonData);
     console.log("done");
-    setlistOfRes(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  }
-console.log(listOfRes)
-  return listOfRes.length===0?<ShimmerUI/>:(
+    setlistOfRes(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFileterlistOfRes(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+  console.log(listOfRes);
+  return listOfRes.length === 0 ? (
+    <ShimmerUI />
+  ) : (
     <div className="body">
-      {/* <div className="search">search</div> */}
+      <div className="searchBox">
+        <input
+          type="text"
+          className="search-input-box"
+          value={searchText}
+          onChange={(e) => {
+            setsearchText(e.target.value);
+          }}
+        />
+        <button
+          className="searchBtn"
+          onClick={() => {
+            const FilteredSearchRes = listOfRes.filter(
+              (res) =>res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFileterlistOfRes(FilteredSearchRes);
+          }}
+        >
+          search
+        </button>
+      </div>
       <div className="btn-div">
-        <button className="filter-bt" onClick={ ()=>{
-          const FilteredList = listOfRes.filter((res)=>res.info.avgRating>4);
-          setlistOfRes(FilteredList);
-        }
-        }>
+        <button
+          className="filter-bt"
+          onClick={() => {
+            const FilteredList = listOfRes.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setlistOfRes(FilteredList);
+          }}
+        >
           top Rated res
         </button>
       </div>
 
       <div className="res-container">
         {/* resturant card */}
-        {listOfRes.map((restur) => (
+        {FileterlistOfRes.map((restur) => (
           <ResturantCard key={restur.info.id} resData={restur} />
         ))}
       </div>
