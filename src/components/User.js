86d3dac0d5b0c083d1bd6ed538a22529e7d4d44b
Kasak Gupta <64642473+palindrome-kasak@users.noreply.github.com>
2024-02-07 03:09:31 +0530

    class comp

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,11 @@
+import {useState} from 'react'
+
 const User = (props)=>{
+    const[count]=useState(0);
     return (
 <div>
     <h2>{props.name}</h2>
+    <h3>count={count}</h3>
     <h2> location: frz</h2>
     <h3>comp: CSG</h3>
 </div>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
+
 class UserClass extends React.Component {
     constructor(props){
         super(props)
+        this.state={
+          count:0,
+          count2:2,
+        }
     }
   render() {
+    const {count2}=this.state;
     return (
       <div>
         <h2>{this.props.name}</h2>
+        <h3>count={this.state.count}</h3>
+        <h3>{count2}</h3>
         <h2> location: frz</h2>
         <h3>comp: CSG</h3>
       </div>
