b7ac51493a752bd2c6e42a8c31ba51d15c1d5ecd
Kasak Gupta <64642473+palindrome-kasak@users.noreply.github.com>
2024-02-13 02:19:38 +0530

    cc

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,12 @@ const Header = () => {
    const onlineStatusValue = useOnlineStatus();
   return (
     
-    <div className="header">
+    <div className="flex justify-between bg-pink">
       <div className="logo-container">
-        <img className="logo " src={LOGO_URL} />
+        <img className="w-100" src={LOGO_URL} />
       </div>
       <div className="nav-items">
-        <ul>
+        <ul className="flex p-4 m-4">
           <li>
             Online Status : {onlineStatusValue?"🟢":"🔴"};
           </li>
